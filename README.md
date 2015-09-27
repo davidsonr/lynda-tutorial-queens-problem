84d58bfb30549bf453c5fdac81d4030282718427
davidsonr <RobertDavidson@Rentalcars.com>
2015-09-27 19:26:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lynda-tutorial-queens-problem
---
+++ src/main/java/com/queens/problem/Greeting.java
@@ -0,0 +1,25 @@
+package com.queens.problem;
+
+/**
+ * Created by davidsonr on 27/09/2015.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+
+}
